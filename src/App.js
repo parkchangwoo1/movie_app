436e8098065be24aa97f0e3dd1216f5519a92f84
parkchangwoo1 <wkwmdsk808@ajou.ac.kr>
2021-05-26 00:09:30 +0900

    Components JSX props example2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-function Food(props) {
-  return <h1>I love {props.fav}</h1>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h1>I love {name}</h1>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const foodIlike = [
+  {
+    name: "Kimchi",
+    image:
+      "https://shop.hansalim.or.kr/im/is/activeDesigner/100101004_content1.jpg",
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://newsimg.hankookilbo.com/cms/articlerelease/2020/06/11/202006111362061920_7.jpg",
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "https://lh3.googleusercontent.com/proxy/q1xAB0EY86lgdf3zVKnAeJtWy0hPOuQgih-5UyuxXdqd0pZxQxPQ_B5CAcByglyyOD4gJyohJoitvMavtx92IfEQLmUigsGsCyI",
+  },
+  {
+    name: "Doncasu",
+    image: "https://www.ypsori.com/news/photo/201811/14515_14466_1033.jpg",
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <h1>Hello!</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi" />
+      {foodIlike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
