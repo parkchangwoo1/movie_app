824e17b1d07d4bfca4383ff9bb92b0ef89d25560
parkchangwoo1 <wkwmdsk808@ajou.ac.kr>
2021-05-25 23:41:47 +0900

    component JSX props example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import Potato from "./Potato";
+function Food(props) {
+  return <h1>I love {props.fav}</h1>;
+}
 
 function App() {
   return (
     <div className="App">
-      Hello!
-      <Potato />
+      <h1>Hello!</h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
     </div>
   );
 }
