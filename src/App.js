659b029368938c297397ff101d17d01dcf69eaac
parkchangwoo1 <wkwmdsk808@ajou.ac.kr>
2021-06-06 23:07:00 +0900

    add: rating

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ const foodIlike = [
     name: "Kimchi",
     image:
       "https://shop.hansalim.or.kr/im/is/activeDesigner/100101004_content1.jpg",
+    rating: 5,
   },
   {
     id: 2,
     name: "Samgyeopsal",
     image:
       "https://newsimg.hankookilbo.com/cms/articlerelease/2020/06/11/202006111362061920_7.jpg",
+    rating: 5,
   },
   {
     id: 3,
     name: "Bibimbap",
     image:
       "https://lh3.googleusercontent.com/proxy/q1xAB0EY86lgdf3zVKnAeJtWy0hPOuQgih-5UyuxXdqd0pZxQxPQ_B5CAcByglyyOD4gJyohJoitvMavtx92IfEQLmUigsGsCyI",
+    rating: 4.9,
   },
   {
     id: 4,
     name: "Doncasu",
     image: "https://www.ypsori.com/news/photo/201811/14515_14466_1033.jpg",
+    rating: 5,
   },
 ];
 function renderFood(dish) {
